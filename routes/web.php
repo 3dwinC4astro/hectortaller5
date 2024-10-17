ef6a3fa5e696ccbac78d3a5f48013b71772a9a1c
Edwin Castro <141517335+3dwinC4astro@users.noreply.github.com>
2024-10-16 22:27:47 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,19 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NoticiaController;
 use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 
+// Rutas de autenticación
 Auth::routes(); // Esto incluye automáticamente las rutas de autenticación, incluyendo logout
 
+// Ruta libre para todos los usuarios
 Route::get('/', [HomeController::class, 'index'])->name('welcome');
-// Ruta para el home
-Route::get('/home', [NoticiaController::class, 'index'])->name('home');
 
-// Rutas RESTful para el recurso 'noticias'
-Route::resource('noticias', NoticiaController::class);
+// Rutas protegidas por autenticación
+Route::group(['middleware' => 'auth'], function () {
+    // Ruta para el home
+    Route::get('/home', [NoticiaController::class, 'index'])->name('home');
+
+    // Rutas RESTful para el recurso 'noticias'
+    Route::resource('noticias', NoticiaController::class);
+});
