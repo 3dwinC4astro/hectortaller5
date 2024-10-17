15c93f79605d154de60179568e998c83174496be
Edwin Castro <141517335+3dwinC4astro@users.noreply.github.com>
2024-10-16 22:47:51 -0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
     <title>Noticias</title>
-    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <link rel="icon" type="image/x-icon" href="{{ secure_asset('assets/favicon.ico') }}" />
     <!-- Font Awesome icons (free version) -->
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     <!-- Google fonts -->
     <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css" />
     <!-- Core theme CSS (includes Bootstrap) -->
-    <link href="css/styles.css" rel="stylesheet" />
+    <link href="{{ secure_asset('css/styles.css') }}" rel="stylesheet" />
 </head>
 <body>
     <!-- Navigation -->
@@ -23,7 +23,7 @@
         </div>
     </nav>
     <!-- Page Header -->
-    <header class="masthead" style="background-image: url('assets/img/home-bg.jpg')">
+    <header class="masthead" style="background-image: url('{{ secure_asset('assets/img/home-bg.jpg') }}')">
         <div class="container position-relative px-4 px-lg-5">
             <div class="row gx-4 gx-lg-5 justify-content-center">
                 <div class="col-md-10 col-lg-8 col-xl-7">
@@ -50,7 +50,7 @@
                         </a>
                         <!-- autor -->
                         <p class="post-meta">
-                            Posted by
+                            publicado por
                             <a href="#!">{{ $noticia->autor }}</a>
                             <!-- fecha de publicación -->
                             on {{ \Carbon\Carbon::parse($noticia->fecha_publicacion)->format('F d, Y') }}
@@ -101,6 +101,6 @@
     <!-- Bootstrap core JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Core theme JS -->
-    <script src="js/scripts.js"></script>
+    <script src="{{ secure_asset('js/scripts.js') }}"></script>
 </body>
 </html>
