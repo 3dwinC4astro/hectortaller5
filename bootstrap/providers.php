ad565a08131640cf507653a41c8c1f7061ef8d85
Edwin Castro <141517335+3dwinC4astro@users.noreply.github.com>
2024-10-16 22:45:06 -0500

    Update providers.php

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -20,7 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-    
+        // Forzar HTTPS en producción
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
         }
     }
 }
